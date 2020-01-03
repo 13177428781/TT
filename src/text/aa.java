6720cf30c4b657ceaaa599434417bb6b79895571
Tedu <Tedu@Tedu-PC>
2020-01-03 20:46:21 +0800

    99

---
+++ src/text/aa.java
@@ -0,0 +1,15 @@
+package text;
+
+public class aa {
+  
+	public static void main(String[] args) {
+		
+		String s ="abc";
+		String bb= "aaaaa";
+		
+	    int a=968;
+	    System.out.println(bb);
+	    ///第三方第三方9999999ppppp
+	}
+	
+}
